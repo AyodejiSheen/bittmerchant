3d322f6b51f5ac491b730da4af6947019837b05a
AyodejiSheen <aoluwasegun009@gmail.com>
2022-03-13 09:40:26 +0100

    ay3

--- src/components/dashboard/layout/home.js
+++ src/components/dashboard/layout/home.js
@@ -70,9 +70,11 @@ const Home = () => {
             </section>
 
 
+
 {/* SECTION 2 */}
 
         <section className='mx-auto md:w-2/3 shadow-md rounded-lg bg-white' data-aos="fade-up ">
+            
         <div className='flex flex-wrap items-center p-3'>
                     <div className='w-1/3'>
                         <video controls className="" style={{width:'100%', height:"100%"}}>
@@ -89,11 +91,11 @@ const Home = () => {
                                 className="mr-3 text-xl  md:mr-0 space-x-4 items-center focus:outline-none dark:focus:ring-gray-600"
                                 id="option-menu-button"
                                 aria-expanded="false"
-                                data-dropdown-toggle="dropdown2"> 
+                                data-dropdown-toggle="dropdown3"> 
                                 <i className="fa-solid fa-ellipsis"></i></button>
 
                                 <div className="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                                id="dropdown2"
+                                id="dropdown3"
                                 >
                                 <div className="py-3 px-4">
                                 <ul className="py-1" aria-labelledby="dropdown">
@@ -104,14 +106,7 @@ const Home = () => {
                                    <i className="fa-solid fa-xmark mr-3 "></i>  Dismiss
                                     </div>
                                 </li>
-                                <li>
-                                    <Link
-                                    to="#"
-                                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                    >
-                                    <i class="fa-solid fa-eye mr-3"></i> View Transcript
-                                    </Link>
-                                </li>
+
                                 <li>
                                     <Link
                                     to="#"
@@ -124,6 +119,7 @@ const Home = () => {
                                 </div>
                             </div>
                             </div>
+                            
                             <div className='px-8 space-y-4'>
                                 <p className='text-base font-thin'>Packaging and shipping your product may not sound like the most glamorous part of running your own business, but it’s one of the most essential aspects of your customer’s experience.</p>
 
